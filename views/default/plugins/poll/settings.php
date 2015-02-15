534a995754efa1752c32d5544a80d05e736e3b25
iionly <iionly@gmx.de>
2015-02-15 21:27:07 +0100

    No longer use private generate_action_token API function

--- views/default/plugins/poll/settings.php
+++ views/default/plugins/poll/settings.php
@@ -19,14 +19,12 @@ $old_polls_count = elgg_get_entities_from_metadata(array(
 $body = '';
 
 if ($old_polls_count > 0) {
-	$ts = time ();
-	$token = generate_action_token ( $ts );
-
 	$body .= elgg_echo('poll:convert:description', array($old_polls_count))."<br><br>";
 	$body .= elgg_view("output/confirmlink", array(
-		'href' => elgg_get_site_url() . "action/poll/convert?__elgg_token=$token&__elgg_ts=$ts",
+		'href' => elgg_get_site_url() . "action/poll/convert",
 		'text' => elgg_echo('poll:convert'),
 		'confirm' => elgg_echo('poll:convert:confirm'),
+		'is_action' => true,
 		'class' => 'elgg-button elgg-button-action'
 	));
 	$body .= "<br><br>";
